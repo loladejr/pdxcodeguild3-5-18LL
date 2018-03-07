4fab7425016f240e2e8f0dd4b4b31eaa28cb7cb4
OloLade Lamina <loladejr@yahoo.com>
2018-03-06 20:25:22 -0800

    eval

---
+++ scalculatorv3.py
@@ -0,0 +1,5 @@
+# num_one = input("Enter whatever basic arithmetric you want solve" ":") #first value entered by customer,
+# print (eval(num_one))
+while True:
+    num_one = input("Enter whatever basic arithmetric you want solve" ":")
+    print (eval(num_one))
